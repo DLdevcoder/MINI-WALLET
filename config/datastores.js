8f85fed5517625541b5de42d84581088563a126d
DLdevcoder <dtlong1410@gmail.com>
2026-06-29 14:00:24 +0700

    chore: setup mongodb replica set and datastore config

--- config/datastores.js
+++ config/datastores.js
@@ -1,6 +1,6 @@
 module.exports.datastores = {
   default: {
-    adapter: "sails-mongo",
-    url: "mongodb://localhost:27017/mini_wallet",
+    adapter: 'sails-mongo',
+    url: 'mongodb://localhost:27017/mini_wallet?replicaSet=rs0',
   },
 };
